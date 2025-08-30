029deff92c865f12d8f0fcff82de5f6a8aa14907
Doug Fletcher <dbfletcher@gmail.com>
2025-08-30 13:09:35 -0400

    CH1-L5 Final

--- main.go
+++ main.go
@@ -6,24 +6,26 @@ import (
 )
 
 func main() {
-	// Create a new ServeMux. A ServeMux is an HTTP request router (or multiplexer).
-	// It compares incoming requests against a list of predefined URL paths,
-	// and calls the associated handler for the path whenever a match is found.
+	// Create a new ServeMux.
 	mux := http.NewServeMux()
 
-	// Create a new http.Server struct. We pass in the port we want to listen on
-	// and the ServeMux we just created.
+	// Create a file server handler that serves files out of the current directory.
+	fileServer := http.FileServer(http.Dir("."))
+
+	// Register the file server handler to the root path "/".
+	// The StripPrefix is important to ensure that the file server looks in the correct directory.
+	mux.Handle("/", fileServer)
+
+	// Create a new http.Server struct.
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 
-	// The ListenAndServe method starts the server. It will block until the server
-	// is stopped, so it should be the last line in your main function.
+	// Start the server.
 	log.Println("Starting server on :8080")
 	err := server.ListenAndServe()
 	if err != nil {
-		// We use log.Fatal to print the error and exit the program.
 		log.Fatal(err)
 	}
 }
